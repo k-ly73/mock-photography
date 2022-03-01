87b7cbfba6b49c3f0d0c2856074162084cc7877a
Kevin Ly <45864296+k-ly73@users.noreply.github.com>
2022-02-28 17:27:26 -0800

    added files

---
+++ mock-photography/src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+class Navbar extends Component {
+    render () {
+        return ( 
+            <div class="navbar-wrapper">
+                <div class="navbar">
+                    <div class="navbar-logo">
+
+                    </div>
+                    <div class="navbar-links">
+                        <div class="nav-item"></div>
+                        <div class="nav-item"></div>
+                    </div>
+                </div>
+            </div>
+        ) 
+    }
+}
---
+++ mock-photography/src/pages/home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class HomePage extends Component {
+    render() {
+        return (
+            <div class="landing-page">
+                <div class="content-section">
+                    
+                </div>
+            </div>
+        )
+    }
+}
+export default HomePage
